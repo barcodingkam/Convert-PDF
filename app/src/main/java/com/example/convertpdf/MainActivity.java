1ddbb8f1bc50baf98227d79d353175c1f0428c67
USER <USER@DESKTOP-CDJRBSG>
2022-03-25 17:11:14 +0800

    hahahahh

---
+++ app/src/main/java/com/example/convertpdf/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.convertpdf;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Environment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText myEditText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myEditText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createMyPDF(view);
+            }
+        });
+    }
+
+
+    public void createMyPDF(View view){
+
+        PdfDocument myPdfDocument = new PdfDocument();
+        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(300,600,1).create();
+        PdfDocument.Page myPage = myPdfDocument.startPage(myPageInfo);
+
+        Paint myPaint = new Paint();
+        String myString = myEditText.getText().toString();
+        int x = 10, y=25;
+
+        for (String line:myString.split("\n")){
+            myPage.getCanvas().drawText(line, x, y, myPaint);
+            y+=myPaint.descent()-myPaint.ascent();
+        }
+
+        myPdfDocument.finishPage(myPage);
+
+        String myFilePath = Environment.getExternalStorageDirectory().getPath() + "/myPDFFile.pdf";
+        File myFile = new File(myFilePath);
+        try {
+            myPdfDocument.writeTo(new FileOutputStream(myFile));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            myEditText.setText("ERROR");
+        }
+
+        myPdfDocument.close();
+    }
+}
